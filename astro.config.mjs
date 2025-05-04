7c73386be6cc7e9106fbcd9da3cf113aa2124c7a
eclopez <erik@eriklopez.net>
2025-05-03 23:05:31 -0700

    feat: Add Tailwind, config, and choose fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Cal Sans',
+        cssVariable: '--font-cal-sans',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
